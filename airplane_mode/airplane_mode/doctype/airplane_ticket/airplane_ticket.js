5353496246e70f8a723c86431b73f9d067127899
Rahul Agrawal <deathstarconsole@Rahuls-MacBook-Air.local>
2025-01-23 08:25:35 +0530

    Day 3 task completed

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, Rl0007 and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+	refresh: function (frm) {
+		// Add a custom button to open the dialog
+		frm.add_custom_button("Assign Seat", () => {
+			// Create a dialog
+			const dialog = new frappe.ui.Dialog({
+				title: "Select Seat",
+				fields: [
+					{
+						label: "Seat Number",
+						fieldname: "seat_number",
+						fieldtype: "Data",
+					},
+				],
+				primary_action_label: "Assign",
+				primary_action: function (data) {
+					// Set the entered seat number to the 'Seat' field in the form
+					frm.set_value("seat", data.seat_number);
+					dialog.hide();
+				},
+			});
 
-// 	},
-// });
+			// Show the dialog
+			dialog.show();
+		}).addClass("btn-primary");
+	},
+});
---
+++ airplane_mode/airplane_mode/doctype/airport/airport.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Rl0007 and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Airport", {
+// 	refresh(frm) {
+
+// 	},
+// });
