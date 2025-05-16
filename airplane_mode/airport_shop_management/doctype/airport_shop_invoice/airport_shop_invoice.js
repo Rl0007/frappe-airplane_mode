73df7f620103e66a96162b12461379b031ae174f
Rahul Agrawal <deathstarconsole@Rahuls-MacBook-Air.local>
2025-05-16 23:43:35 +0530

    fix: remove comments from contract detail

--- airplane_mode/airport_shop_management/doctype/airport_shop_invoice/airport_shop_invoice.js
+++ airplane_mode/airport_shop_management/doctype/airport_shop_invoice/airport_shop_invoice.js
@@ -3,34 +3,13 @@
 
 frappe.ui.form.on("Airport Shop Invoice", {
 	refresh(frm) {
-		// frm.add_custom_button("Mark as Paid",()=>{
-		//     frm.set_value("status","Paid")
-		//     frm.save()
-		// })
 		if (frm.get_value("status") !== "Unpaid") {
 			frm.add_custom_button("Mark as Paid", () => {
-				// // Create a dialog
-				// const dialog = new frappe.ui.Dialog({
-				// 	title: "Mark as Paid",
-
-				// 	primary_action_label: "Mark as Paid",
-				// 	primary_action: function (data) {
-				// 		// Set the entered seat number to the 'Seat' field in the form
-				// 		// frm.set_value("seat", data.seat_number);
-				//         frm.set_value("status","Paid")
-				//             frm.save()
-				// 		dialog.hide();
-				// 	},
-				// });
-
-				// // Show the dialog
-				// dialog.show();
 				frappe.msgprint({
 					title: __("Mark as Paid"),
 					message: __("Are you sure you want to proceed?"),
 					primary_action: {
 						action(values) {
-							// console.log(values);
 							const date = new Date();
 							frm.set_value("status", "Paid");
 							frm.set_value("payment_date", date);
--- airplane_mode/airport_shop_management/doctype/contract_detail/contract_detail.js
+++ airplane_mode/airport_shop_management/doctype/contract_detail/contract_detail.js
@@ -10,62 +10,12 @@
 // 	},
 // });
 
-// frappe.ui.form.on("Airport Shop Contract", {
-//     refresh: function(frm) {
-//         console.log("hello world")
-//     },
-//     shop_details_remove: function(frm) {
-//         console.log("hwllo world")
-//         frappe.msgprint(__('A row has been deleted from the child table!'));
-//         // You code here
-//         // If you console.log(frm.doc.color) you will get the remaining color list
-
-//         },
-// });
 frappe.ui.form.on("Contract Detail", {
 	validate: function (frm) {
 		calculate_total_rent(frm);
 	},
-	// shops_add: function(frm, cdt, cdn) {
-	//     console.log("hello world")
-	//     frappe.db.get_single_value('Airport Shop Settings', 'default_rent').then(value => {
-	//         frappe.model.set_value(cdt, cdn, 'rent', value);
-	//     });
-	// }
-	shops_on_form_rendered: function (frm, grid_row, cdt, cdn) {
-		console.log("hello world");
-		if (grid_row.doc.__islocal) {
-			console.log("hello world");
-			frappe.db.get_single_value("Airport Shop Settings", "default_rent").then((value) => {
-				frappe.model.set_value(cdt, cdn, "rent", value);
-			});
-		}
-	},
-});
-frappe.ui.form.on("Airport Shop Contract", {
-	// this should be the child doctype name
-	form_render: function (frm, cdt, cdn) {
-		console.log("hello world");
-	},
-	refresh: function (frm) {
-		console.log("hello world");
-	},
-	shop: function (frm, cdt, cdn) {
-		// frappe.db.get_single_value('Airport Shop Settings', 'default_rent').then(value => {
-		//     frappe.model.set_value(cdt, cdn, 'rent', value);
-		// });
-		calculate_total_rent(frm);
-	},
 });
 
-// frappe.ui.form.on('Shops',{
-//     shop: function(frm)
-// {
-//     console.log("hello world")
-// }   , rent: function(frm, cdt, cdn) {
-//         calculate_total_rent(frm);
-//     }
-// })
 function calculate_total_rent(frm) {
 	let ttl = 0;
 	frm.doc.shops.forEach(function (s) {
@@ -73,9 +23,3 @@ function calculate_total_rent(frm) {
 	});
 	frm.set_value("rent_amount", ttl);
 }
-
-// frappe.ui.form.on('Airport Shop Contract', {
-//     shop_remove: function(frm){
-//         console.log("helo world")
-//     }
-// })
